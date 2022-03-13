8d6bc3f930214d29c8c33b5d4d7d2ddfdcf43adb
D Dranidis <dranidis@gmail.com>
2022-03-14 01:26:21 +0200

    Work with lists within maps

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ A Clojure library to transform clojure vectors to maps.
 
 (vector->map [2 3 4]) 
 
+## Library creation
+
+https://github.com/clojars/clojars-web/wiki/Tutorial
--- src/vector_to_map/core.cljs
+++ src/vector_to_map/core.cljs
@@ -4,7 +4,7 @@
 ;; Utility functions for transforming lists to maps
 ;; Firebase returns a list when the keys are numbers 0, 1, 2
 ;;
-(defn vector->map
+(defn- vector->map
   "Tranforms a vector of elements to a map with the stringified indices as keys.
    e.g. [0 3 4] => {\"0\": 0 \"1\": 3 \"2\": 4}"
   ([list] (vector->map list 0))
@@ -12,8 +12,24 @@
    (if (empty? list) {}
        (assoc (vector->map (rest list) (inc n)) (keyword (str n)) (first list)))))
 
+(declare if-vector?->map)
+
+(defn- work-on-map
+  [a-map]
+  (if (= {} a-map)
+    a-map
+    (apply merge (map
+                  (fn [el] {(first el) (if-vector?->map (second el))})
+                  (seq a-map)))))
+
 (defn if-vector?->map
   [value]
-  (if (vector? value)
-    (vector->map value)
-    value))
+  (cond
+    (vector? value) (vector->map value)
+    (map? value) (work-on-map value)
+    :else value))
+
+
+
+
+
