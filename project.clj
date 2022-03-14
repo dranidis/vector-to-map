45578b2e5a43c46f53034851439d9b70953ee333
Dimitris Dranidis <dranidis@gmail.com>
2022-03-14 13:08:16 +0200

    Project settings

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.clojars.dranidis/vector-to-map "0.1.3-SNAPSHOT"
-  :description "Transforms a vector to a map"
+  :description "Transforms vectors to a maps with indices as keys, for usage with firebase."
   :url "https://github.com/dranidis/vector-to-map"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
